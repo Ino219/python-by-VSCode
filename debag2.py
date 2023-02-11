9ed1c3d0ca2f1adea0cc5d31a14d27b55c95961b
Ino <ainstain1220@gmail.com>
2023-02-11 19:51:25 +0900

    add

--- debag2.py
+++ debag2.py
@@ -1,5 +1,13 @@
 #debag2
 
+import random
+
+cmpNameList=["c12","c13","c14","c15","c16"]
+PinNameList=["c12_1","c12_2","c13_1","c13_2","c14_1","c14_2","c15_1","c15_2","c16_1","c16_2"]
+
+tempX=[]
+tempY=[]
+
 c16_X=[]
 c16_Y=[]
 
@@ -7,6 +15,36 @@ c16_1_loc=[3,4]
 c16_2_loc=[8,4]
 c16_pin=[c16_1_loc,c16_2_loc]
 
+
+#2要素で乱数リストを返すメソッド
+def randomList(pinName):
+    x=random.randrange(20)
+    y=random.randrange(20)
+    xy=[x,y]
+    return xy
+#部品リストとピンを照合し、部品の座標を書き付ける処理
+outputFile=open("sample.txt","w")
+for cmpName in cmpNameList:
+    tempX=[]
+    tempY=[]
+    for PinName in PinNameList:
+        tempPin=[]
+        
+        if cmpName in PinName:
+            tempPin.append(PinName)
+            #ピン名の登録
+            tempXY=randomList(PinName)
+            tempX.append(tempXY[0])
+            tempY.append(tempXY[1])
+    xMAX=max(tempX)
+    xMIN=min(tempX)
+    yMAX=max(tempY)
+    yMIN=min(tempY)
+    line=cmpName+","+str(xMAX)+","+str(xMIN)+","+str(yMAX)+","+str(yMIN)
+    print(line)
+    outputFile.writelines(line)
+outputFile.close()
+        
 #create rectangle
 #座標取得
 #一つのコンポーネントからピンを抽出し、それぞれのピンの座標をリストに収める
